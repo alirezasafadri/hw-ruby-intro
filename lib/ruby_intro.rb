e9f32b05ae129d875edde9c87dd79c251580b57a
EC2 Default User <ec2-user@ip-172-31-6-235.ec2.internal>
2021-02-04 07:35:58 +0000

    hw1 is completed and tested

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,53 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  arr.inject(0){|sum,x| sum + x }
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  arr.max(2).inject(0){|sum,x| sum + x }
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  arr.combination(2).detect{|x,y| x + y == n} != nil
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  'Hello, ' + name
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  # (!!(s[0] =~ /[a-z]/i)) && (!!(s[0] =~ /[^aeiou]/i))
+  !!(s[0] =~ /(?=[a-z])(?=[^aeiou])/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  (/^[10]*00$/.match(s) != nil) || (s == "0")
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError.new(
+     "isbn should not be an empty string!"
+    ) if isbn.to_s.strip.empty?() == true
+  
+    raise ArgumentError.new(
+     "price should be more than 0!"
+    ) if price.positive?() == false
+  
+      @isbn = isbn
+      @price = price
+  end
+  
+  def price_as_string
+    "$#{'%.2f' % @price.round(2)}"
+  end
 end
